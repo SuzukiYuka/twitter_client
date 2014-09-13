42f73f9cd5bf424faaa5928b5b0dd996fab944d6
SUZUKIyuka <suzukiyuka1218@gmail.com>
2014-09-14 07:50:32 +0900

    Initial Commit

---
+++ MyTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTwitter
+//
+//  Created by nagata on 9/14/14.
+//  Copyright (c) 2014 Suzuki Yuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
